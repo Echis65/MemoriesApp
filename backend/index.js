040d6f0edbeebb1377a3c8ef3397e48005a6b5ea
Echis65 <evansatompoya@gmial.com>
2021-10-19 12:31:58 +0000

    deleting redundant memories folder and connection or database to server

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,9 @@ app.use( bodyParser.json( { limit: "30mb", extended: true } ) )
 app.use( bodyParser.urlencoded( { limit: "30mb", extended: true } ) )
 
 app.use( cors() );
+
+const CONNECTION_URL = 'mongodb+srv://echis65:Echisviper65@cluster0.jva0l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect( CONNECTION_URL, { useUrlPaser: true } )
