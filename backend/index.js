51071e6f562aea9f0475edccc7a347a73ce9166f
unknown <evansatompoya@gmail.com>
2022-03-15 17:01:35 +0000

    Deployed Frontend and Backend to Netlify and Heroku Respectively

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,10 @@ app.use( bodyParser.urlencoded( { limit: "30mb", extended: true } ) )
 app.use( cors() );
 app.use( "/posts", postRoutes )
 
+app.get("/", (req, res) => {
+    res.send("Welcome to Memories Api")
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect( process.env.CONNECTION_URL, { useUnifiedTopology: true } )
